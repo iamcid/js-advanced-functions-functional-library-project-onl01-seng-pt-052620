e1716276cfb5e8f9053c5ab3765228119c07a71a
iamcid <iamcid17@gmail.com>
2021-01-03 01:36:13 -0500

    commit tree 06a88814a5414267c0fad402ae4c2a2c54c7bee8

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let newCollection = (collection instanceof Array) ? collection : Object.values(collection)
+      for (let i = 0; i < newCollection.length; i++){
+        callback(newCollection[i])
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newCollection = [];
+      let newerCollection = (collection instanceof Array) ? collection : Object.values(collection)
+      for (let i = 0; i < newerCollection.length; i++){
+        newCollection.push(callback(newerCollection[i]))
+      }
+      return newCollection
     },
 
     reduce: function() {
